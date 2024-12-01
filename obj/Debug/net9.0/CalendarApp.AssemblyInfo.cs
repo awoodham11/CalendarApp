0f378b0aea01c9a0eecb536196bce8460f9c9235
unknown <alexwoodham08@gmail.com>
2024-12-01 21:20:53 +0000

    added link from 'view all events' page

--- obj/Debug/net9.0/CalendarApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/CalendarApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalendarApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09d882e4b998653f2b9576765eb37516620baa65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b7b1f279a9b978ae88060ecb5d7faede524bfc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalendarApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalendarApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
